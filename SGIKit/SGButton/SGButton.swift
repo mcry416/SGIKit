a595a2ae8acc5e3e28c202c0989f6f516b85cc2c
mcry416 <50766712+mcry416@users.noreply.github.com>
2022-02-16 10:48:25 +0800

    Add files via upload

--- SGIKit/SGButton/SGButton.swift
+++ SGIKit/SGButton/SGButton.swift
@@ -1,8 +1,8 @@
 //
 //  SGButton.swift
-//  NewNavigationBar
+//  SGIKit
 //
-//  Created by MengQingyu iMac on 2022/2/10.
+//  Created by MCRY416 iMac on 2021/12/19.
 //
 
 import UIKit
--- SGIKit/SGButton/SGButtonTypealias.swift
+++ SGIKit/SGButton/SGButtonTypealias.swift
@@ -1,20 +1,23 @@
 //
 //  SGButtonTypealias.swift
-//  NewNavigationBar
+//  SGIKit
 //
-//  Created by MengQingyu iMac on 2022/2/10.
+//  Created by MCRY416 iMac on 2021/12/19.
 //
 
 import UIKit
 
-/**
- Set on SGButton click listener. Corresponding to it is touchUpInside in UIButton.Event.
- - Returns : Void.
- */
-typealias SetOnButtonClickListener = () -> Void
+extension SGButton {
+    
+    /**
+     Set on SGButton click listener. Corresponding to it is touchUpInside in UIButton.Event.
+     - Returns: Void.
+     */
+    typealias SetOnButtonClickListener = () -> Void
 
-/**
- Set on SGButton touch listener. Corresponding to it is touchDown in UIButton.Event.
- - Returns : Void.
- */
-typealias SetOnButtonTouchListener = () -> Void
+    /**
+     Set on SGButton touch listener. Corresponding to it is touchDown in UIButton.Event.
+     - Returns: Void.
+     */
+    typealias SetOnButtonTouchListener = () -> Void
+}
