6caab2d8c41e72a9e661800a93fa9aaa50fbf871
mcry416 <50766712+mcry416@users.noreply.github.com>
2022-05-28 21:38:11 +0800

    Add files via upload

---
+++ SGIKit/SGFoundation/SGString/SGSize/SGSize.swift
@@ -0,0 +1,17 @@
+//
+//  SGSize.swift
+//  Compent
+//
+//  Created by Eldest's MacBook on 2022/5/21.
+//
+
+import UIKit
+
+/// UI widgets size
+class SGSize {
+    
+    public static func size(_ view: UIView) -> CGSize{
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
+    
+}
