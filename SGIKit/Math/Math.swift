9a03360b6c4a67c3fde8e20e22586485b0596612
mcry416 <50766712+mcry416@users.noreply.github.com>
2023-04-16 17:34:10 +0800

    Update Math.

--- SGIKit/Math/Math.swift
+++ SGIKit/Math/Math.swift
@@ -10,5 +10,33 @@ import UIKit
 class Math: NSObject {
     
     public static var pi: Double { Double.pi }
+    
+    public static var randomCondition: Bool {
+        arc4random() % 2 == 0
+    }
+    
+    public static var randomInt: Int { Int(arc4random()) }
+    
+    public static var randomFloat: Double { drand48() }
+    
+    public static func randomInt(end: Int) -> Int{
+        Int(arc4random_uniform(UInt32(end)))
+    }
+    
+    public static func maxs(others: Int...){
+        
+    }
+    
+    public static func mins(others: Int...){
+        
+    }
+    
+    public static func maxs(others: Float...){
+        
+    }
+    
+    public static func mins(others: Float...){
+        
+    }
 
 }
